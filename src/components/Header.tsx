2538a5a2f1bdaa36c4e09f377dde26f16e376a4e
Jack Shunn <jack.shunn@gmail.com>
2025-07-19 12:19:04 -0600

    fix tests

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router';
 import { useCart } from '../context/CartContext';
 import { currentConfig } from '../config/foodCartConfig';
 import './Header.css';
